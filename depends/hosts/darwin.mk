81941f5576ef0c1dbf4596809662f6234c1e7496
Tom E <8545471+tomevoll@users.noreply.github.com>
2018-06-03 03:33:33 +0200

    no message

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -3,15 +3,15 @@ OSX_SDK_VERSION=10.9
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=241.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -I/usr/include/c++/5/ -I/usr/include/x86_64-linux-gnu/c++/5/ -I/home/ubuntu/build/stonecoin/depends/x86_64-apple-darwin11/native/include
+darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
 darwin_CFLAGS=-pipe
-darwin_CXXFLAGS=$(darwin_CFLAGS) -I/usr/include/c++/5/ -I/usr/include/x86_64-linux-gnu/c++/5/ -I/home/ubuntu/build/stonecoin/depends/x86_64-apple-darwin11/native/include
+darwin_CXXFLAGS=$(darwin_CFLAGS)
 
 darwin_release_CFLAGS=-O2
-darwin_release_CXXFLAGS=$(darwin_release_CFLAGS) -I/usr/include/c++/5/ -I/usr/include/x86_64-linux-gnu/c++/5/ -I/home/ubuntu/build/stonecoin/depends/x86_64-apple-darwin11/native/include
+darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 
 darwin_debug_CFLAGS=-O1
-darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS) -I/usr/include/c++/5/ -I/usr/include/x86_64-linux-gnu/c++/5/ -I/home/ubuntu/build/stonecoin/depends/x86_64-apple-darwin11/native/include
+darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
 darwin_native_toolchain=native_cctools
