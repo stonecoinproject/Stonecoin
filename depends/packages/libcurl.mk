0ecf7642c0c028baa982befa81f77a165b4f9f94
Tom E <8545471+tomevoll@users.noreply.github.com>
2018-06-13 05:34:40 +0100

    fix build fails on 32bit

--- depends/packages/libcurl.mk
+++ depends/packages/libcurl.mk
@@ -1,8 +1,8 @@
 package=libcurl
-$(package)_version=7.47.0
+$(package)_version=7.60.0
 $(package)_download_path=http://pool.erikosoftware.org/updater/
 $(package)_file_name=curl-$($(package)_version).tar.gz
-$(package)_sha256_hash=df01bd42af361978d9de7de8529718bcafe01897a544a7650139a1954c55bdfe
+$(package)_sha256_hash=e9c37986337743f37fd14fe8737f246e97aec94b39d1b71e8a5973f72a9fc4f5
 
 define $(package)_set_vars
 $(package)_config_opts_release=
@@ -13,7 +13,7 @@ $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared --with-
 $(package)_config_opts_mingw32=--with-winssl --host=x86_32-w32-mingw32 --target=x86_32-w32-mingw32 CPPFLAGS="-DCURL_STATICLIB -DWIN32"
 $(package)_config_opts_x86_64_mingw32=--with-winssl --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32  CPPFLAGS="-DCURL_STATICLIB -DWIN32"
 $(package)_config_opts_i686_mingw32=--with-winssl --host=i686_mingw32 --target=i686_mingw32  CPPFLAGS="-DCURL_STATICLIB -DWIN32"
-$(package)_config_opts_i686_linux=--with-ssl --host= x86_64-pc-linux-gnu --target= x86_64-pc-linux-gnu CPPFLAGS="-DCURL_STATICLIB"
+$(package)_config_opts_i686_linux=--with-ssl --host=i686-pc-linux-gnu --target=i686-pc-linux-gnu CPPFLAGS="-DCURL_STATICLIB -m32" LDFLAGS="-m32" CFLAGS="-m32"
 $(package)_toolset_$(host_os)=gcc
 $(package)_archiver_$(host_os)=$($(package)_ar)
 $(package)_toolset_darwin=darwin
